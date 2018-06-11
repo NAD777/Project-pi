7a5b2e4f2e20cf813815d3c3510a38f09a80554b
NAD777 <nehaev07.07.2003@gmail.com>
2018-06-11 19:25:34 +0300

    add to client rtc and delay

--- client_gpio.py
+++ client_gpio.py
@@ -1,18 +1,30 @@
 import socket
 import RPi.GPIO as GPIO
 import SimpleMFRC522
+from datetime import datetime
+from time import sleep
 
-reader = SimpleMFRC522.SimpleMFRC522()
+def get_time(): #get current time and date 
+	now = datetime.now()
+	now_str = datetime.strftime(now, "%Y.%m.%d %H:%M:%S") 
+	return now_str
+def read_rfid(): # read rfid tag 
+	reader = SimpleMFRC522.SimpleMFRC522() 
+	id, text = reader.read()
+	return text
 
 sock = socket.socket()
 sock.connect(('192.168.1.187',9090))
+
 try:
 	while(True):
-		id, text = reader.read()
+		
 		id = str(id)
-		sock.send(id.encode('utf-8'))
+		date_for_sent_by_socket_to_server = get_time()+" "+read_rfid() 
+		sock.send(date_for_sent_by_socket_to_server.encode('utf-8'))
 		data = sock.recv(1024)
 		print(data.decode())
+		sleep(1)
 finally:
 	GPIO.cleanup()
 	sock.close()
