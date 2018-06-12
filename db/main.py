05cc61310986b6547d8d403f7ceadb87bd88e967
NAD777 <nehaev07.07.2003@gmail.com>
2018-06-12 19:51:51 +0300

    add class for data base

--- db/main.py
+++ db/main.py
@@ -1,33 +1,38 @@
 import sqlite3
 from person import Person
+class db(Person):
+    def __init__(self,Person):
+        self.Person1=Person
+        self.conn = sqlite3.connect('person.db')  # person.db
+        self.c = self.conn.cursor()
+        self.time = '12'
+        self.id = 'Mark'
+        self.status = 0
 
-def commit(Person1):
-    c.execute("INSERT INTO person VALUES (:time,:id,:in_school)",
-              {'time': Person1.time, 'id': Person1.id, 'in_school': Person1.in_school})
-
-    conn.commit()
-def get_date(colomn,person):
-    c.execute("SELECT * FROM person WHERE "+colomn+'='+"'"+person+"'") # c.execute("SELECT * FROM  person WHERE id = 'Ilya'")
-    print(c.fetchall())
-    conn.commit()
-
-conn = sqlite3.connect('person.db') #person.db
-
-c = conn.cursor()
+    def commit(self,Person1):
+        self.c.execute("INSERT INTO person VALUES (:time,:id,:in_school)",
+                  {'time': Person1.time, 'id': Person1.id, 'in_school': Person1.in_school})
+        self.conn.commit()
+    def get_date(self,colomn,person):
+        self.c.execute("SELECT * FROM person WHERE "+colomn+'='+"'"+person+"'") # c.execute("SELECT * FROM  person WHERE id = 'Ilya'")
+        print(self.c.fetchall())
+        self.conn.commit()
+    def close(self):
+            self.conn.close()
 
 #c.execute("""CREATE TABLE  person (
 #    time text,
 #    id text,
 #    in_school  integer
 #)""")
-time = '12'
-id='Mark'
+
+time = '13:12'
+id = 'Mark'
 status = 0
 Person1 = Person(time, id, status)
-#commit(Person1)
+database = db(Person1)
+database.commit(Person1)
+database.get_date('id','Ilya')
+database.close()
 
-get_date('id','Ilya')
 
-
-
-conn.close()
