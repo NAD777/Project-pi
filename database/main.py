a55f1ab339de7012d5edac765cdb38efac17e3dd
NAD777 <nehaev07.07.2003@gmail.com>
2018-06-12 11:58:20 +0300

    add database not connected to other script :D

---
+++ database/main.py
@@ -0,0 +1,31 @@
+import sqlite3
+from person import Person
+
+def commit(Person1):
+    c.execute("INSERT INTO person VALUES (:time,:id,:in_school)",
+              {'time': Person1.time, 'id': Person1.id, 'in_school': Person1.in_school})
+
+    conn.commit()
+
+conn = sqlite3.connect('person.db') #person.db
+
+c = conn.cursor()
+
+#c.execute("""CREATE TABLE  person (
+#    time text,
+#    id text,
+#    in_school  integer
+#)""")
+time = '11'
+id='Mark'
+status = 0
+Person1 = Person(time, id, status)
+commit(Person1)
+
+c.execute("SELECT * FROM  person WHERE id = 'Ilya'")
+
+print(c.fetchall())
+
+conn.commit()
+
+conn.close()
---
+++ database/person.py
@@ -0,0 +1,5 @@
+class Person:
+    def __init__(self,time,id,in_school):
+        self.time=time
+        self.id=id
+        self.in_school=in_school
