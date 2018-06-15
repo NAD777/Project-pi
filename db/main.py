4d6400be3a96165336af5dde59264e3b576f74e3
NAD777 <nehaev07.07.2003@gmail.com>
2018-06-15 22:15:42 +0300

    add test site for json

--- db/main.py
+++ db/main.py
@@ -1,21 +1,17 @@
 import sqlite3
 from person import Person
-class db(Person):
-    def __init__(self,Person):
-        self.Person1=Person
+class db:
+    def __init__(self):
         self.conn = sqlite3.connect('person.db')  # person.db
-        self.c = self.conn.cursor()
-        self.time = '12'
-        self.id = 'Mark'
-        self.status = 0
+        self.cursor = self.conn.cursor()
 
-    def commit(self,Person1):
-        self.c.execute("INSERT INTO person VALUES (:time,:id,:in_school)",
-                  {'time': Person1.time, 'id': Person1.id, 'in_school': Person1.in_school})
+    def commit(self,Person):
+        self.cursor.execute("INSERT INTO person VALUES (:time,:id,:in_school)",
+                  {'time': Person.time, 'id': Person.id, 'in_school': Person.in_school})
         self.conn.commit()
     def get_date(self,colomn,person):
-        self.c.execute("SELECT * FROM person WHERE "+colomn+'='+"'"+person+"'") # c.execute("SELECT * FROM  person WHERE id = 'Ilya'")
-        print(self.c.fetchall())
+        self.cursor.execute("SELECT * FROM person WHERE "+colomn+'='+"'"+person+"'") # c.execute("SELECT * FROM  person WHERE id = 'Ilya'")
+        print(self.cursor.fetchall())
         self.conn.commit()
     def close(self):
             self.conn.close()
---
+++ site/script.js
@@ -0,0 +1,11 @@
+function sayHi(massage) {
+	document.write('<p class=\"brd\">',massage,'</p>')
+}
+function alert_date()
+{
+	var mydata = JSON.parse(data);
+	alert(mydata[0].name);
+	alert(mydata[0].age);
+	alert(mydata[1].name);
+	alert(mydata[1].age);
+}
